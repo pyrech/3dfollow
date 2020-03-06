0f540861bfe647bb5279ec707dbb73974105893a
Loïck Piera <pyrech@gmail.com>
2020-03-06 02:00:16 +0100

    Add legal content

--- application/src/Controller/HomeController.php
+++ application/src/Controller/HomeController.php
@@ -15,4 +15,20 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    /**
+     * @Route("/terms", name="home_terms", methods={"GET"})
+     */
+    public function terms(): Response
+    {
+        return $this->render('home/terms.html.twig');
+    }
+
+    /**
+     * @Route("/privacy", name="home_privacy", methods={"GET"})
+     */
+    public function privacy(): Response
+    {
+        return $this->render('home/privacy.html.twig');
+    }
 }
